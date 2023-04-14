913df2f286f5a44a2d4234ef7ec3f8187ad490a7
akhbarona <akhbarona.118140193@studente.itera.ac.id>
2023-04-14 15:36:04 +0700

    fix: penghapusan beberapa code yang di comment

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-// use Faker\Factory as Faker;
 
 class AdminSeeder extends Seeder
 {
@@ -16,61 +15,6 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // $faker = Faker::create('id_ID');
-        // $admin = [
-        //     [
-        //         'role_id' => 1,
-        //         'nama_depan' => 'Super',
-        //         'nama_belakang' => 'Admin',
-        //         'email' => 'superadmin@moeliadesign.id',
-        //         'phone_number' => $faker->phoneNumber,
-        //         'password' => bcrypt('superadmin'),
-        //     ],
-        //     [
-        //         'role_id' => 2,
-        //         'nama_depan' => 'Admin',
-        //         'nama_belakang' => 'Admin',
-        //         'email' => 'admin@moeliadesign.id',
-        //         'phone_number' => $faker->phoneNumber,
-        //         'password' => bcrypt('admin'),
-        //     ],
-        //     [
-        //         'role_id' => 2,
-        //         'nama_depan' => 'Hastio',
-        //         'nama_belakang' => 'Wahyu',
-        //         'email' => 'hastiowahyu@moeliadesign.id',
-        //         'phone_number' => $faker->phoneNumber,
-        //         'password' => bcrypt('admin'),
-        //     ],
-        // ];
-        // foreach ($admin as $key => $value) {
-        //     Admin::create($value);
-        // }
-
-        // $faker = Faker::create('id_ID');
-
-        // for ($i = 0; $i < 50; $i++) {
-        //     $nama_depan = $faker->firstName;
-        //     $nama_belakang = $faker->lastName;
-
-        //     while (strlen($nama_depan) < 5) {
-        //         $nama_depan = $faker->firstName;
-        //     }
-
-        //     while (strlen($nama_belakang) < 5) {
-        //         $nama_belakang = $faker->lastName;
-        //     }
-
-        //     Admin::create([
-        //         'nama_depan' => $nama_depan,
-        //         'nama_belakang' => $nama_belakang,
-        //         'role_id' => 2,
-        //         'email' => $faker->email,
-        //         'phone_number' => $faker->phoneNumber,
-        //         'password' => bcrypt('admin')
-        //     ]);
-        // }
         Admin::factory()->count(50)->create();
     }
 }
