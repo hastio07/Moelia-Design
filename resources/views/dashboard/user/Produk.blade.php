5cff5e561665ee3669ec79eeb532dd4b5cc1e706
hastio07 <hastiowahyuu@gmail.com>
2023-04-17 14:44:02 +0700

    add icon catogories dan icon date on product card

--- resources/views/dashboard/user/Produk.blade.php
+++ resources/views/dashboard/user/Produk.blade.php
@@ -22,11 +22,19 @@
                             <img src="img/cover-2.jpg" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
-                            <div class="card-body">
+                            <div class="card-body pb-0">
                                 <h5 class="card-title">Ervin Howell</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est?</p>
+                                <div class="category d-flex">
+                                    <i class="bi bi-calendar-event d-flex me-2">
+                                        <p class="ms-1">20/1/2023</p>
+                                    </i>
+                                    <i class="bi bi-tag-fill ms-2 d-flex">
+                                        <p class="ms-1">Make Up</p>
+                                    </i>
+                                </div>
                             </div>
-                            <div class="btn btn ms-3 mt-3">selengkapnnya</div>
+                            <div class="btn btn ms-3">selengkapnnya</div>
                         </div>
                     </div>
                 </div>
@@ -38,11 +46,19 @@
                             <img src="img/cover-2.jpg" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
-                            <div class="card-body">
+                            <div class="card-body pb-0">
                                 <h5 class="card-title">Ervin Howell</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est?</p>
+                                <div class="category d-flex">
+                                    <i class="bi bi-calendar-event d-flex me-2">
+                                        <p class="ms-1">20/1/2023</p>
+                                    </i>
+                                    <i class="bi bi-tag-fill ms-2 d-flex">
+                                        <p class="ms-1">Make Up</p>
+                                    </i>
+                                </div>
                             </div>
-                            <div class="btn btn ms-3 mt-3">selengkapnnya</div>
+                            <div class="btn btn ms-3">selengkapnnya</div>
                         </div>
                     </div>
                 </div>
@@ -54,11 +70,19 @@
                             <img src="img/cover-2.jpg" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
-                            <div class="card-body">
+                            <div class="card-body pb-0">
                                 <h5 class="card-title">Ervin Howell</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est?</p>
+                                <div class="category d-flex">
+                                    <i class="bi bi-calendar-event d-flex me-2">
+                                        <p class="ms-1">20/1/2023</p>
+                                    </i>
+                                    <i class="bi bi-tag-fill ms-2 d-flex">
+                                        <p class="ms-1">Make Up</p>
+                                    </i>
+                                </div>
                             </div>
-                            <div class="btn btn ms-3 mt-3">selengkapnnya</div>
+                            <div class="btn btn ms-3">selengkapnnya</div>
                         </div>
                     </div>
                 </div>
@@ -70,11 +94,19 @@
                             <img src="img/cover-2.jpg" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
-                            <div class="card-body">
+                            <div class="card-body pb-0">
                                 <h5 class="card-title">Ervin Howell</h5>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est?</p>
+                                <div class="category d-flex">
+                                    <i class="bi bi-calendar-event d-flex me-2">
+                                        <p class="ms-1">20/1/2023</p>
+                                    </i>
+                                    <i class="bi bi-tag-fill ms-2 d-flex">
+                                        <p class="ms-1">Make Up</p>
+                                    </i>
+                                </div>
                             </div>
-                            <div class="btn btn ms-3 mt-3">selengkapnnya</div>
+                            <div class="btn btn ms-3">selengkapnnya</div>
                         </div>
                     </div>
                 </div>
