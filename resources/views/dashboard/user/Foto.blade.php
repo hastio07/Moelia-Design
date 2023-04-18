301606702520bf1d1ee3a2cbafbd0f89c432a82f
akhbarona <akhbarona.118140193@studente.itera.ac.id>
2023-04-19 01:08:12 +0700

    fix: mengluarkan value variable photos

--- resources/views/dashboard/user/Foto.blade.php
+++ resources/views/dashboard/user/Foto.blade.php
@@ -2,41 +2,35 @@
 @section('title', 'Home')
 
 @push('styles')
-<link href="{{ asset('templates') }}/assets/css-modif/Home-Gallery.css" rel="stylesheet">
+    <link href="{{ asset('templates') }}/assets/css-modif/Home-Gallery.css" rel="stylesheet">
 @endpush
 @section('konten')
-<section>
-    <div class="head-gallery text-center align-self-center">
-        <h1>Gallery Foto</h1>
-        <p> Home/Gallery/Foto</p>
-    </div>
-    <div class="container gallery mt-5 mb-5">
-        <div class="row">
-            <a href="https://unsplash.it/1200/768.jpg?image=251" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=251" class="img-fluid">
-            </a>
-            <a href="https://unsplash.it/1200/768.jpg?image=252" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=252" class="img-fluid">
-            </a>
-            <a href="https://unsplash.it/1200/768.jpg?image=253" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=253" class="img-fluid">
-            </a>
+    <section>
+        <div class="head-gallery text-center align-self-center">
+            <h1>Gallery Foto</h1>
+            <p> Home/Gallery/Foto</p>
         </div>
-        <div class="row mt-4">
-            <a href="https://unsplash.it/1200/768.jpg?image=254" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=254" class="img-fluid">
-            </a>
-            <a href="https://unsplash.it/1200/768.jpg?image=255" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=255" class="img-fluid">
-            </a>
-            <a href="https://unsplash.it/1200/768.jpg?image=256" data-toggle="lightbox" data-gallery="photo-gallery" class="col-sm-4">
-                <img src="https://unsplash.it/600.jpg?image=256" class="img-fluid">
-            </a>
+        <div class="container mt-5 mb-5">
+            <div class="row g-2">
+                @if (!empty($photos))
+                    @foreach ($photos as $key => $value)
+                        <div class="col-sm-4">
+                            <a data-gallery="photo-gallery" data-toggle="lightbox" href="/storage/{{ $value->photo_path }}">
+                                <img alt="foto-{{ $key }}" class="img-fluid" height="600" src="/storage/{{ $value->photo_path }}" width="600">
+                            </a>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="text-center">
+                        <h2>Foto Tidak ada</h2>
+                    </div>
+                @endif
+            </div>
+            {!! $photos->links('pagination::bootstrap-5') !!}
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
 @endpush
