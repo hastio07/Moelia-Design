62668e9cc8cadbc26e619a6f2e8ba96006898507
akhbarona <akhbarona.118140193@studente.itera.ac.id>
2023-04-19 01:10:30 +0700

    fix: mengluarkan nilai variable products

--- resources/views/dashboard/user/DetailItem.blade.php
+++ resources/views/dashboard/user/DetailItem.blade.php
@@ -2,25 +2,26 @@
 @section('title', 'Detail Item')
 
 @push('styles')
-<link href="{{ asset('templates') }}/assets/css-modif/DetailItem.css" rel="stylesheet">
+    <link href="{{ asset('templates') }}/assets/css-modif/DetailItem.css" rel="stylesheet">
 @endpush
 
 @section('konten')
-<section class="container details-item d-flex align-items-center">
-    <div class="card mx-auto mb-3">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="img/cover-2.jpg" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <section class="container details-item d-flex align-items-center">
+        <div class="card mx-auto mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img alt="{{ $products->slug_produk }}" class="img-fluid rounded-start" src="/storage/post-images/{{ $products->gambar }}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $products->nama_produk }}</h5>
+                        <p class="card-text">{!! $products->deskripsi !!}</p>
+                        <p class="card-text">{!! $products->rincian_produk !!}</p>
+                        <p class="card-text"><small class="text-muted">{{ $products->updated_at->diffForHumans() }}</small></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 @endsection
