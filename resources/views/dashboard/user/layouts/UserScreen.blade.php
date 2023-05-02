b42b25f9d0608e630a08fd7f7172d8f14e07fe8e
akhbarona <akhbarona.118140193@studente.itera.ac.id>
2023-05-02 23:54:13 +0700

    fix: memperbaiki class active navbar user

--- resources/views/dashboard/user/layouts/UserScreen.blade.php
+++ resources/views/dashboard/user/layouts/UserScreen.blade.php
@@ -50,25 +50,37 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item">
-                            <a aria-current="page" class="nav-link active" href="/">Home</a>
+                            <a @class(['nav-link', 'active' => Route::is('home')]) aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/produk">Produk</a>
+                            <a @class(['nav-link', 'active' => Route::is('produk.*')]) href="/produk">Produk</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a aria-expanded="false" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">
+                            <a @class([
+                                'nav-link',
+                                'dropdown-toggle',
+                                'active' => Route::is('foto') || Route::is('vidio'),
+                            ]) aria-expanded="false" data-bs-toggle="dropdown" href="#" role="button">
                                 Gallery
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/foto">Foto</a></li>
-                                <li><a class="dropdown-item" href="/vidio">Vidio</a></li>
+                                <li><a @class([
+                                    'nav-link',
+                                    'dropdown-item',
+                                    'bg-light active' => Route::is('foto'),
+                                ]) href="/foto">Foto</a></li>
+                                <li><a @class([
+                                    'nav-link',
+                                    'dropdown-item',
+                                    'bg-light active' => Route::is('vidio'),
+                                ]) href="/vidio">Vidio</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/maintenance">Wedding Calculator</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/aboutus">Tentang Kami</a>
+                            <a @class(['nav-link', 'active' => Route::is('aboutus')]) href="/aboutus">Tentang Kami</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/maintenance">Pembayaran</a>
