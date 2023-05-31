25074866eaa2769393d8b16e88b51eb8472de0f8
Akhbarona <akhbarona.118140193@student.itera.ac.id>
2023-05-31 21:29:48 +0700

    fix: hapus css & js - rename beberapa css

--- public/templates/assets/js/page/crudgallery.js
+++
@@ -1,6 +0,0 @@
-const deleteModal = document.getElementById('DeleteModal');
-deleteModal.addEventListener('show.bs.modal', (event) => {
-    const button = event.relatedTarget;
-    const route = button.getAttribute('data-bs-route');
-    deleteModal.querySelector('.modal-content .modal-footer > form').setAttribute('action', route);
-});
