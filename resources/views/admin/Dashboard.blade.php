e3c7930c7a85ca1ff53478e53b9d3b66eb2cd646
hastio07 <hastiowahyuu@gmail.com>
2023-06-06 00:46:14 +0700

    minor

--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -5,17 +5,6 @@
 @endpush
 
 @section('content')
-<div class="dropdown">
-    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Dropdown link
-    </a>
-
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-</div>
 <div class="dashboard">
     <div class="content-wrapper">
         <div class="row same-height">
