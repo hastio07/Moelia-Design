de1fda274bad58b8d142c6d2df5e8f915dbfe6bf
hastio07 <hastiowahyuu@gmail.com>
2023-06-06 21:16:40 +0700

    minor

--- resources/views/user/Vidio.blade.php
+++ resources/views/user/Vidio.blade.php
@@ -2,24 +2,24 @@
 @section('title', 'Gallery Vidio')
 
 @push('styles')
-    <link href="{{ asset('templates') }}/assets/css-modif/Home-Gallery.css" rel="stylesheet">
+<link href="{{ asset('templates') }}/assets/css-modif/user/UserGallery.css" rel="stylesheet">
 @endpush
 @section('konten')
-    <section>
-        <div class="head-gallery text-center align-self-center">
-            <h1>Gallery Vidio</h1>
-            <div class="d-flex justify-content-center">
-                {{ Breadcrumbs::render('vidio') }}
-            </div>
+<section>
+    <div class="head-gallery text-center align-self-center">
+        <h1>Gallery Vidio</h1>
+        <div class="d-flex justify-content-center">
+            {{ Breadcrumbs::render('vidio') }}
         </div>
-        <div class="container gallery mt-5 mb-5">
-            <div class="row">
-                @foreach ($videos as $value)
-                    <a class="col-sm-3 mb-4" data-gallery="youtubevideos" data-toggle="lightbox" href="{{ $value->video_path }}">
-                        <img alt="{{ $value->video_name }}" class="img-fluid" src="{{ $value->video_thumbnail }}">
-                    </a>
-                @endforeach
-                {{-- <a href="https://youtu.be/iQ4D273C7Ac?start=30" data-toggle="lightbox" data-gallery="youtubevideos" class="col-sm-3 mb-4">
+    </div>
+    <div class="container gallery mt-5 mb-5">
+        <div class="row">
+            @foreach ($videos as $value)
+            <a class="col-sm-3 mb-4" data-gallery="youtubevideos" data-toggle="lightbox" href="{{ $value->video_path }}">
+                <img alt="{{ $value->video_name }}" class="img-fluid" src="{{ $value->video_thumbnail }}">
+            </a>
+            @endforeach
+            {{-- <a href="https://youtu.be/iQ4D273C7Ac?start=30" data-toggle="lightbox" data-gallery="youtubevideos" class="col-sm-3 mb-4">
                 <img src="https://i1.ytimg.com/vi/iQ4D273C7Ac/mqdefault.jpg" class="img-fluid">
             </a>
             <a href="//www.youtube.com/embed/dQw4w9WgXcQ" data-toggle="lightbox" data-gallery="youtubevideos" class="col-sm-3 mb-4">
@@ -31,12 +31,12 @@
             <a href="//www.youtube.com/embed/dQw4w9WgXcQ" data-toggle="lightbox" data-gallery="youtubevideos" class="col-sm-3 mb-4">
                 <img src="https://i1.ytimg.com/vi/dQw4w9WgXcQ/mqdefault.jpg" class="img-fluid">
             </a> --}}
-            </div>
-            {!! $videos->links('pagination::bootstrap-5') !!}
         </div>
-    </section>
+        {!! $videos->links('pagination::bootstrap-5') !!}
+    </div>
+</section>
 @endsection
 
 @push('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
 @endpush
