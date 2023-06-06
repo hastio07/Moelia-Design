9a694c4f24021787e455570899423f041055ce7c
hastio07 <hastiowahyuu@gmail.com>
2023-06-06 21:19:11 +0700

    minor

--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -12,7 +12,7 @@
                 <div class="card-body">
                     <div class="col-md-10 mx-auto">
                         <div class="row ">
-                            <div class="col-xl-3 col-lg-6">
+                            <div class="col-xl-3 col-lg-6 col-md-6">
                                 <div class="card l-bg-cherry">
                                     <div class="card-statistic-3 p-4">
                                         <div class="card-icon card-icon-large"><i class="fas fa-shopping-cart"></i></div>
@@ -35,7 +35,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-lg-6">
+                            <div class="col-xl-3 col-lg-6 col-md-6">
                                 <div class="card l-bg-blue-dark">
                                     <div class="card-statistic-3 p-4">
                                         <div class="card-icon card-icon-large"><i class="fas fa-users"></i></div>
@@ -58,7 +58,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-lg-6">
+                            <div class="col-xl-3 col-lg-6 col-md-6">
                                 <div class="card l-bg-green-dark">
                                     <div class="card-statistic-3 p-4">
                                         <div class="card-icon card-icon-large"><i class="fas fa-ticket-alt"></i></div>
@@ -81,7 +81,30 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-lg-6">
+                            <div class="col-xl-3 col-lg-6 col-md-6">
+                                <div class="card l-bg-orange-dark">
+                                    <div class="card-statistic-3 p-4">
+                                        <div class="card-icon card-icon-large"><i class="fas fa-dollar-sign"></i></div>
+                                        <div class="mb-4">
+                                            <h5 class="card-title mb-0">Revenue Today</h5>
+                                        </div>
+                                        <div class="row align-items-center mb-2 d-flex">
+                                            <div class="col-8">
+                                                <h2 class="d-flex align-items-center mb-0">
+                                                    $11.61k
+                                                </h2>
+                                            </div>
+                                            <div class="col-4 text-right">
+                                                <span>2.5% <i class="fa fa-arrow-up"></i></span>
+                                            </div>
+                                        </div>
+                                        <div class="progress mt-1 " data-height="8" style="height: 8px;">
+                                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="25" class="progress-bar l-bg-cyan" data-width="25%" role="progressbar" style="width: 25%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-6 col-md-6">
                                 <div class="card l-bg-orange-dark">
                                     <div class="card-statistic-3 p-4">
                                         <div class="card-icon card-icon-large"><i class="fas fa-dollar-sign"></i></div>
