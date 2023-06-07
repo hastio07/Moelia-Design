ce9033780bfb037b1c8b97736a48bbe9cd5b814b
hastio07 <hastiowahyuu@gmail.com>
2023-06-07 15:25:17 +0700

    fixed detail produk

--- resources/views/user/DetailItem.blade.php
+++ resources/views/user/DetailItem.blade.php
@@ -9,9 +9,6 @@
 <section class="container p-5 shadow detail-item">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-5">
-                <img alt="{{ $products->slug_produk }}" class="img-fluid rounded-start" src="/storage/post-images/{{ $products->gambar }}">
-            </div>
             <div class="col-md-7">
                 <h4>{{ $products->nama_produk }}</h4>
                 <div class="price text-success fw-bold mt-4"><i class="bi bi-tags-fill me-1"></i>{{$products->formatRupiah('harga_sewa')}}</div>
@@ -23,7 +20,12 @@
                     <h6 class="fw-bold">Rician Produk</h6>
                     <p>{!! $products->rincian_produk !!}</p>
                 </div>
-                <div class="d-flex align-items-center mt-5 delivery">{{ $products->updated_at->diffForHumans() }}</div>
+                <div class="d-flex align-items-center mt-5 delivery">
+                    <div class="d-flex">
+                        <i class="bi bi-clock-history me-2"></i>
+                        <p>{{ $products->updated_at->diffForHumans() }}</p>
+                    </div>
+                </div>
                 <hr>
                 <div class="d-flex flex-row kategori">
                     <i class="bi bi-calendar-event-fill d-flex me-2">
@@ -34,7 +36,33 @@
                     </i>
                 </div>
             </div>
+            <div class="col-md-5">
+                <img alt="{{ $products->slug_produk }}" class="img-fluid rounded" src="/storage/post-images/{{ $products->gambar }}">
+                <div class="row mt-1 photo-product">
+                    <div class="col-lg-4 col-md-4 col-sm-6 mt-2 px-2">
+                        <a data-gallery="photo-gallery" data-toggle="lightbox" href="/storage/post-images/{{ $products->gambar }}">
+                            <img alt="{{ $products->slug_produk }}" class="img-fluid rounded" height="600" src="/storage/post-images/{{ $products->gambar }}" width="600">
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 mt-2 px-2">
+                        <a data-gallery="photo-gallery" data-toggle="lightbox" href="/storage/post-images/{{ $products->gambar }}">
+                            <img alt="{{ $products->slug_produk }}" class="img-fluid rounded" height="600" src="/storage/post-images/{{ $products->gambar }}" width="600">
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 mt-2 px-2">
+                        <a data-gallery="photo-gallery" data-toggle="lightbox" href="/storage/post-images/{{ $products->gambar }}">
+                            <img alt="{{ $products->slug_produk }}" class="img-fluid rounded" height="600" src="/storage/post-images/{{ $products->gambar }}" width="600">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </section>
+
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
+@endpush
+
 @endsection
