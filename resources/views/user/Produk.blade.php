ddf009cfae11eafb4d6b20f41c3d347c92d8a647
hastio07 <hastiowahyuu@gmail.com>
2023-06-10 04:02:25 +0700

    add image data not found

--- resources/views/user/Produk.blade.php
+++ resources/views/user/Produk.blade.php
@@ -49,7 +49,10 @@
             @if ($products->isEmpty())
             <div class="container d-flex justify-content-center align-items-center" style="height: 50vh;">
                 <div class="text-center">
-                    <h5 class="fw-bold text-secondary">Opss!! Produk Saat Ini Belum Tesedia!!</h5>
+                    <div class="container">
+                        <img src="{{ asset('templates') }}/assets/images/data-kosong.jpg" class="img-fluid" alt="Phone image" style="max-width: 200px; max-height: 200px;">
+                    </div>
+                    <h5 class="fw-bold text-secondary mt-2">Opss!! Produk Saat Ini Belum Tesedia!!</h5>
                     <a class="btn btn-warning mt-3" href="/"><i class="bi bi-arrow-left me-2"></i> Back To Home</a>
                 </div>
             </div>
@@ -109,7 +112,6 @@
             </div>
             @endforeach
             @endif
-
             <div class="px-5 pt-4 text-center">
                 {!! $products->links('pagination::bootstrap-5') !!}
             </div>
