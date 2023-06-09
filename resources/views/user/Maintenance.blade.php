195520cab4ed441f55f8c5b8feaf722a481a80ae
hastio07 <hastiowahyuu@gmail.com>
2023-06-09 23:10:37 +0700

    minor

--- resources/views/user/Maintenance.blade.php
+++ resources/views/user/Maintenance.blade.php
@@ -1,29 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta content="IE=edge" http-equiv="X-UA-Compatible">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link href="{{ asset('templates') }}/assets/css-modif/UserMaintenance.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css" rel="stylesheet">
-    <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" rel="stylesheet">
-    <title>Document</title>
+    <style>
+        .pegawai {
+            position: relative;
+            width: 50%;
+        }
+
+        .image-pegawai {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+
+        .overlay-pegawai {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background-color: #008CBA;
+            overflow: hidden;
+            width: 100%;
+            height: 0;
+            transition: .5s ease;
+        }
+
+        .pegawai:hover .overlay-pegawai {
+            height: 100%;
+        }
+
+        .text {
+            white-space: nowrap;
+            color: white;
+            font-size: 20px;
+            position: absolute;
+            overflow: hidden;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+        }
+    </style>
 </head>
 
 <body>
-    <div class="wrapper text-center">
-        <h1>coming soon<span class="dot">.</span></h1>
-        <p>maaf menu ini sedang dalam masa perbaikan</p>
-        <div class="icons">
-            <a href="/">
-                <button class="btn btn-primary">Home<i class="bi bi-box-arrow-up-right ms-2"></i></button>
-            </a>
+
+    <h2>Slide in Overlay from the Bottom</h2>
+
+    <div class="pegawai">
+        <img src="{{ asset('templates') }}/assets/images/time.jpg" alt="Avatar" class="image-pegawai">
+        <div class="overlay-pegawai">
+            <div class="text">Hello World</div>
         </div>
     </div>
-</body>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
 
 </html>
