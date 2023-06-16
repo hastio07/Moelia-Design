c9afc8c9ba825eb9319c4a7c0bedd6b16eb369b6
hastio07 <hastiowahyuu@gmail.com>
2023-06-16 15:21:12 +0700

    ubah tampilan aja

--- resources/views/admin/ProfileAdmin.blade.php
+++ resources/views/admin/ProfileAdmin.blade.php
@@ -1,81 +1,70 @@
-<!DOCTYPE html>
-<html lang="id">
+@extends('admin.layouts.layouts')
+@section('title', 'Profile')
 
-<head>
-    <meta charset="UTF-8">
-    <meta content="IE=edge" http-equiv="X-UA-Compatible">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" rel="stylesheet">
-    <title>Pengaturan Akun</title>
-</head>
-
-<body>
-    <section class="d-flex align-items-center justify-content-center min-vh-100">
-        <div class="container py-5">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card mb-4">
-                        <div class="card-body text-center">
-                            <img alt="avatar" class="rounded-circle img-fluid" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" style="width: 150px;">
-                            <h5 class="my-3">John Smith</h5>
-                            <p class="text-muted mb-1">Super Admin</p>
-                            <div class="justify-content-center mb-2 mt-4">
-                                <button class="btn btn-primary" type="button"><i class="bi bi-pencil-square"></i> Ubah Biodata</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <a href="{{ route('dashboard') }}">
-                        <button class="btn btn-primary mb-3"><i class="bi bi-arrow-left"></i> Dashboard</button>
-                    </a>
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <p class="mb-0">Nama</p>
-                                </div>
-                                <div class="col-sm-9">
-                                    <p class="text-muted mb-0">Johnatan Smith</p>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <p class="mb-0">Email</p>
-                                </div>
-                                <div class="col-sm-9">
-                                    <p class="text-muted mb-0">example@example.com</p>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <p class="mb-0">No. Handphone</p>
-                                </div>
-                                <div class="col-sm-9">
-                                    <p class="text-muted mb-0">082376545678</p>
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-sm-3">
-                                    <p class="mb-0">Role</p>
-                                </div>
-                                <div class="col-sm-9">
-                                    <p class="text-muted mb-0">Admin</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+@section('content')
+<div class="container card mb-4 p-4">
+    <h5 class="fw-bold ms-3">Pengaturan Akun</h5>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-3">
+                <p class="mb-0">Nama Depan</p>
+            </div>
+            <div class="col-lg-8">
+                <p class="text-muted mb-0">Johnatan Smith</p>
+            </div>
+            <div class="col-lg-1">
+                <button class="btn shadow-0"><i class="bi bi-pencil"></i></button>
             </div>
         </div>
-    </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.js"></script>
-</body>
-
-</html>
+        <hr>
+        <div class="row">
+            <div class="col-lg-3">
+                <p class="mb-0">Nama Belakang</p>
+            </div>
+            <div class="col-lg-8">
+                <p class="text-muted mb-0">Johnatan Smith</p>
+            </div>
+            <div class="col-lg-1">
+                <button class="btn shadow-0"><i class="bi bi-pencil"></i></button>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-3">
+                <p class="mb-0">Email</p>
+            </div>
+            <div class="col-lg-8">
+                <p class="text-muted mb-0">example@example.com</p>
+            </div>
+            <div class="col-lg-1">
+                <button class="btn shadow-0"><i class="bi bi-pencil"></i></button>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-3">
+                <p class="mb-0">No. Handphone</p>
+            </div>
+            <div class="col-lg-8">
+                <p class="text-muted mb-0">082376545678</p>
+            </div>
+            <div class="col-lg-1">
+                <button class="btn shadow-0"><i class="bi bi-pencil"></i></button>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-3">
+                <p class="mb-0">Password</p>
+            </div>
+            <div class="col-lg-8">
+                <p class="text-muted mb-0">**********</p>
+            </div>
+            <div class="col-lg-1">
+                <button class="btn shadow-0"><i class="bi bi-pencil"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
