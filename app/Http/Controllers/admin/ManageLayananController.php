e3763c5dd8f50ba55260b44c2190a48d3f32adb6
Akhbarona <akhbarona.118140193@student.itera.ac.id>
2023-07-26 02:58:11 +0700

    fix: image source not readable 2

--- app/Http/Controllers/admin/ManageLayananController.php
+++ app/Http/Controllers/admin/ManageLayananController.php
@@ -97,8 +97,8 @@ class ManageLayananController extends Controller
             $fileName = basename($oriPath);
 
             // kompres gambar dan simpan ke folder penyimpanan
-            $thumbImage = ImageResize::make('public_html/storage/' . $oriPath);
-            $thumbPath = 'public_html/compressed/' . $fileName;
+            $thumbImage = ImageResize::make(public_path('storage/' . $oriPath));
+            $thumbPath = public_path('public_html/compressed/' . $fileName);
             $thumbImage->save($thumbPath, 20);
             $data['gambar'] = $fileName;
         }
