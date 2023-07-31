61d38e90cc59a39cd2b4266da293004d37023dc7
hastio07 <hastiowahyuu@gmail.com>
2023-07-31 21:00:18 +0700

    fixed waktu pembayaran dp dan fp

--- resources/views/user/Pembayaran.blade.php
+++ resources/views/user/Pembayaran.blade.php
@@ -176,19 +176,17 @@
                             <div class="row mt-2">
                                 <div class="col-6">Tgl Pembayaran:</div>
                                 <div class="col-6">
-                                    {!! $bayar_dp->waktu_pembayaran !!}
                                     @if($bayar_dp->jenis_pembayaran === 'dp' && $bayar_dp->waktu_pembayaran)
                                     Waktu Pembayaran: {{ $bayar_dp->waktu_pembayaran }}
                                     @else
                                     Pembayaran belum dilakukan atau menggunakan metode pembayaran lain.
                                     @endif
-
                                 </div>
 
                             </div>
                             <div class="row mt-2">
                                 <div class="col-6">Waktu Pembayaran:</div>
-                                <div class="col-6">{!! $bayar_dp->waktu_pembayaran ?? '<p class="text-dark fw-bold">-</p>' !!}</div>
+                                {{-- <div class="col-6">{!! $bayar_dp->waktu_pembayaran ?? '<p class="text-dark fw-bold">-</p>' !!}</div> --}}
                             </div>
                         </div>
                         <hr>
@@ -196,7 +194,13 @@
                             <h6 class="fw-bold">Tahap 2 (Pelunasan)</h6>
                             <div class="row mt-2">
                                 <div class="col-6">Tgl Pembayaran:</div>
-                                <div class="col-6"> {!! $bayar_dp->waktu_pembayaran ?? '<p class="text-dark fw-bold">-</p>' !!}</div>
+                                <div class="col-6">
+                                    @if($bayar_dp->jenis_pembayaran === 'fp' && $bayar_dp->waktu_pembayaran)
+                                    Waktu Pembayaran: {{ $bayar_dp->waktu_pembayaran }}
+                                    @else
+                                    Pembayaran belum dilakukan atau menggunakan metode pembayaran lain.
+                                    @endif
+                                </div>
                             </div>
                             <div class="row mt-2">
                                 <div class="col-6">Waktu Pembayaran:</div>
